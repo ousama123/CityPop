db7bc5fafa2248faf417129cf5f3abb81d91ee8d
Oussama Anadani <anadani@student.chalmers.se>
2021-09-04 17:05:41 +0200

    Added cities list when searched by country and fixed the result page

--- src/components/PopResult.js
+++ src/components/PopResult.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 function PopResult(props) {
+  console.log(props);
   return (
     <div className="main">
       <div className="container">
@@ -8,7 +9,7 @@ function PopResult(props) {
           <h4>{props.location.state.valueInput}</h4>
           <div className="population">
             <div>Population</div>
-            <h2>{props.location.state.populationResult}</h2>
+            <h2>{props.location.state.population}</h2>
           </div>
         </div>
       </div>
